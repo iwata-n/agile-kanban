068e2c90169bf6ec877a76dbb69a8fcd91d0ee55
Iwata Naoki <a.tokai.wain@gmail.com>
2016-02-16 17:44:24 +0900

    Reactのタッチイベントを有効化

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Router, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history';
 
 import AppRoutes from './Routes';
+
+injectTapEventPlugin();
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
