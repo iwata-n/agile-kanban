8fbc0ba2140e866fd70311e1bfa10635ab9bcb5e
Iwata Naoki <a.tokai.wain@gmail.com>
2016-02-17 07:35:36 +0900

    ドラッグ＆ドロップでカンバンを移動できるUIの実装

--- src/components/Dashboard/StoryKanban.jsx
+++ src/components/Dashboard/StoryKanban.jsx
@@ -1,22 +1,113 @@
 import React from 'react';
+import $ from "jquery";
 
 import TaskKanban from "./TaskKanban";
 
-const StoryKanban = React.createClass({
-  render() {
-    const tasks = this.props.tasks.map( (task, i) => {
+import Paper from 'material-ui/lib/paper';
+import GridList from 'material-ui/lib/grid-list/grid-list';
+import GridTile from 'material-ui/lib/grid-list/grid-tile';
+import Badge from 'material-ui/lib/badge';
+
+
+const styles = {
+  root: {
+    position: 'relative',
+    width: '100%',
+    padding: 5,
+    margin: 5,
+  },
+  state: {
+    width: '30%',
+    display: 'inline-block',
+    verticalAlign: 'top',
+    padding: 5,
+    margin: 5,
+  }
+
+};
+
+/**
+ * ストーリーごとにカンバンを束ねる為のカンバン
+ */
+export default class StoryKanban extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tasks: []
+    }
+  }
+  
+  componentWillMount() {
+    $.ajax({
+        method: 'GET',
+        url: '/api/story/' + this.props.id,
+        cache: false,
+        success: (data) => {
+          if (data.result === 'ok') {
+            this.setState({tasks: data.message})
+          } else {
+            this.props.onError(data.message)
+          }
+        },
+        error: (xhr, message, err) => {
+          this.props.onError(err)
+        }
+      })
+  }
+  
+  handleDragStart(event) {
+    event.dataTransfer.setData("text", event.target.id)
+  }
+  
+  handleDragOver(event) {
+    event.preventDefault()
+  }
+  
+  handleDrop(event) {
+    var id = event.dataTransfer.getData("text")
+    
+    var drag_elm =document.getElementById(id);
+    //ドロップ先にドラッグされた要素を追加
+    event.currentTarget.appendChild(drag_elm);
+    event.preventDefault()
+  }
+  
+  taskList(state) {
+    const filtered_tasks = this.state.tasks.filter((element) => {
+      return (element.state == state)
+    })
+    const tasks = filtered_tasks.map( (task, i) => {
       return (
-        <TaskKanban key={i} title={task.title} assign={task.assign} state={task.state} description={task.description} />
+        <TaskKanban
+            key={i}
+            id={task.id}
+            title={task.title}
+            assign={task.assign}
+            state={task.state}
+            description={task.description}
+            onDragStart={this.handleDragStart.bind(this)}/>
       );
     });
+    const title = (state)
     return (
-      <div>
-        <div>{this.props.title}</div>
-        <div>{this.props.id}</div>
+      <Paper style={styles.state}
+             className={state}
+             onDragOver={this.handleDragOver.bind(this)}
+             onDrop={this.handleDrop.bind(this)}>
+        {title}
         {tasks}
-      </div>
+      </Paper>
+    )
+  }
+
+  render() {
+    return (
+      <Paper style={styles.root} zDepth={1}>
+        <div>#{this.props.id} {this.props.title}</div>
+        {this.taskList('todo')}
+        {this.taskList('doing')}
+        {this.taskList('done')}
+      </Paper>
     );
   }
-});
-
-export default StoryKanban;
+}
--- src/components/Dashboard/TaskKanban.jsx
+++ src/components/Dashboard/TaskKanban.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
-const TaskKanban = React.createClass({
+import Card from 'material-ui/lib/card/card';
+import CardActions from 'material-ui/lib/card/card-actions';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardText from 'material-ui/lib/card/card-text';
+import FlatButton from 'material-ui/lib/flat-button';
+import Paper from 'material-ui/lib/paper';
+
+const style = {
+  background: '#FFFFCC',
+  padding: 5,
+  margin: 5,
+};
+
+export default class TaskKanban extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
-      <div>
-        <div>{this.props.title}</div>
-        <div>{this.props.assign}</div>
-        <div>{this.props.description}</div>
-      </div>
+        <Paper style={style}
+               id={this.props.id}
+               draggable="true"
+               onDragStart={this.props.onDragStart}>
+          {this.props.title}
+        </Paper>
     );
   }
-});
-
-export default TaskKanban;
+}
