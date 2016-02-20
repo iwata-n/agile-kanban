3db0b4b71cd0521a655cd04070b312eb68dec938
Iwata Naoki <a.tokai.wain@gmail.com>
2016-02-20 19:19:05 +0900

    ルーティングを変更しWebAPIの雛形を追加

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var story = require('./routes/story');
+var task = require('./routes/task');
 
 var app = express();
 
@@ -22,7 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', routes);
+app.use('/api/story', story);
+app.use('/api/task', task);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
